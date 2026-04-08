8b2baf9ede3a20d25c6991c98a48be3338174f8b
Yoshitaka Kamesaka <kamejimateria@gmail.com>
2026-04-08 19:43:48 +0900

    fileup

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/kamejimateria-portfolio/", // ← GitHub Pages のパス
   plugins: [react()],
   server: {
     watch: {
